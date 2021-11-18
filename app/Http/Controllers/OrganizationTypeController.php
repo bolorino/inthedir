b8e5fbc73bf3e6f659b7f2a407d7cff36cd86efc
Jose Bolorino <jose.bolorino@gmail.com>
2021-11-18 12:38:14 +0100

    Apply state styles

--- app/Http/Controllers/OrganizationTypeController.php
+++ app/Http/Controllers/OrganizationTypeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Organization;
 use App\Models\OrganizationType;
 use App\Models\Province;
-use http\Env\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
