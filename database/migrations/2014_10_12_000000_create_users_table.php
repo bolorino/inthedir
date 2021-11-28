a0c2160c3efc9027e67a34ceba3a8a256226e9a5
Jose Bolorino <jose.bolorino@gmail.com>
2021-11-28 01:31:52 +0100

    Merged migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('enabled')->default(0);
+            $table->timestamp('approved_at')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2021_09_30_171127_create_organizations_table.php
+++ database/migrations/2021_09_30_171127_create_organizations_table.php
@@ -16,6 +16,7 @@ class CreateOrganizationsTable extends Migration
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('province_id')->constrained('provinces');
+            $table->foreignId('type_id')->default(1)->constrained('organization_types');
             $table->string('name', 140)->unique();
             $table->string('slug', 155)->unique();
             $table->string('address', 200);
@@ -24,6 +25,9 @@ class CreateOrganizationsTable extends Migration
             $table->string('postal_code', 5);
             $table->string('phone', 15)->nullable();
             $table->string('website', 125)->nullable();
+            $table->string('email', 45)->nullable();
+            $table->string('image', 155)->nullable();
+            $table->string('logo', 155)->nullable();
             $table->timestamps();
         });
     }
